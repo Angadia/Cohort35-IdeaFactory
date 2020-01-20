dec609a697c7ea04e6e05081d7f273d78abda498
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2020-01-20 13:13:09 -0800

    Added routes for resources Ideas paths edit, update, and destroy; Added Ideas controller actions show, edit, update, and destroy; Added Idea show, and edit view pages; Refactored new, and edit view pages with partials.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,6 @@
 class IdeasController < ApplicationController
+  before_action :find_idea, only: [:show, :edit, :update, :destroy]
+
   def index
     @ideas = Idea.all.order(updated_at: :desc)
   end
@@ -18,8 +20,36 @@ class IdeasController < ApplicationController
     end
   end
 
-  private
+  def show
+    
+  end
 
+  def edit
+
+  end
+
+  def update
+    if @idea.update idea_params
+      flash[:notice] = "Idea Updated Successfully"
+      redirect_to @idea
+    else
+      flash.now[:alert] = @idea.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  def destroy
+    @idea.destroy
+    flash[:notice] = "Idea Deleted Successfully"
+    redirect_to root_path
+  end
+
+  private
+  
+  def find_idea
+    @idea = Idea.find(params[:id])
+  end
+  
   def idea_params
     params.require(:idea).permit(:title, :description)
   end
